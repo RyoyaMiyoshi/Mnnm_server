81ef1c591b4f9b0d2d6188ae0fae6d116301fe5e
Satoshi Morohara <g2116048@fun.ac.jp>
2016-11-06 01:07:49 +0900

    画像から文章を書き出せるようにした

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var http = require( 'http' ); // HTTPモジュール読み込み
 var socketio = require( 'socket.io' ); // Socket.IOモジュール読み込み
 var fs = require( 'fs' ); // ファイル入出力モジュール読み込み
 var pg = require( 'pg' );
+var Tesseract = require('tesseract.js')
 
    //ポート固定でHTTPサーバーを立てる
      var server = http.createServer( function( req, res ) {
@@ -22,7 +23,7 @@ var pg = require( 'pg' );
      res.end();
      });
    });
-   server.listen(process.env.PORT)
+   server.listen(process.env.PORT || 3000)
 
 var io = socketio.listen(server);
 var connect_db = "postgres://ayeypgykvzakah:_17U8W-o_m4Q2fXT-XxulAbxIN@ec2-54-235-254-199.compute-1.amazonaws.com:5432/d5g401imvq4lac";
@@ -33,8 +34,8 @@ io.sockets.on('connection',function(socket){
 
 socket.on('encode',function( data ) {
 pg.connect(connect_db,function(err, client){
- console.log("connect db");  
-var pcid = "select id from notes"; 
+ console.log("connect db");
+var pcid = "select id from notes";
 
 client.query(pcid, function(err, max)
  {
@@ -48,36 +49,41 @@ client.query(pcid, function(err, max)
  });
 
 
-socket.on('list',function(){
-pg.connect(connect_db,function(err, client){
- console.log("connect db");
-var imax = "select id from notes;"
-client.query(imax,function(err, max){
-console.log(max.rows.length);
-var i;
-var w = 0;
-var q=max.rows.length;
-var array = new Array();
-for(i=q;i>q-8;i=i-1){  
-var getdata = "select id, pdf from notes where id = "+i+";"  
-client.query(getdata,function(err, note){
-array[w] = new Object();
-array[w].code = note.rows[0].pdf;
-array[w].id = note.rows[0].id;
-console.log(array[w].id);
-w = w+1;
-});
-}
-io.sockets.emit('list_back',array);
-console.log("success");
-});
-});
+// socket.on('list',function(){
+// pg.connect(connect_db,function(err, client){
+//  console.log("connect db");
+// var imax = "select id from notes;"
+// client.query(imax,function(err, max){
+// console.log(max.rows.length);
+// var i;
+// var w = 0;
+// var q=max.rows.length;
+// var array = new Array();
+// for(i=q;i>q-8;i=i-1){
+// var getdata = "select id, pdf from notes where id = "+i+";"
+// client.query(getdata,function(err, note){
+// array[w] = new Object();
+// array[w].code = note.rows[0].pdf;
+// array[w].id = note.rows[0].id;
+// console.log(array[w].id);
+// w = w+1;
+// });
+// }
+// io.sockets.emit('list_back',array);
+// console.log("success");
+// });
+// });
+// });
+
+socket.on("convert_img", function(note_img){
+  Tesseract.recognize(note_img, {lang:"jpn"}).then(function(result){
+    console.log("finish ocr");
+    socket.emit("convert_text", result.html);
+  });
 });
 
 
 
-
-  
 });
 
 
